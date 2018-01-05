28366399f1196034a6af8130770cce742a727bf1
Sumit Sarkar <hitman3r44@gmail.com>
2018-01-06 05:26:00 +0600

    Update the app

---
+++ src/main/java/com/wolverinesolution/springbootstarter/CourseApiApp.java
@@ -0,0 +1,12 @@
+package com.wolverinesolution.springbootstarter;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+    public static void main(String[] args){
+
+    }
+}
